4cc2b153deb9b5318a948b773f211ae9d0c308ec
Ashley Leyva <aleyva2@nd.edu>
2025-07-02 09:35:06 -0700

    Added environments.js with Parse keys

---
+++ src/environments.js
@@ -0,0 +1,6 @@
+// Copied as indicated in the lecture
+module.exports = {
+    APPLICATION_ID: "MgmWYQAix8vz9UwMpT2hlmIfYPOmrPezwGcF12al",
+    JAVASCRIPT_KEY: "j9ABdeBysO8JtmmNzvQaobJWqeAzql63i4UYaUqd",
+    SERVER_URL: "https://parseapi.back4app.com/"
+};
