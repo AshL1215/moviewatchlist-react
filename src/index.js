60a8d2ba1e6631b6cd2998502a48beab09d17938
Confidence Nawali <137927899+Confidence85@users.noreply.github.com>
2025-07-02 18:31:56 -0700

    Fixed Ordering in index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-// Parse Initialization
-// Importing environment variables
-
-// Initialize Parse
-Parse.initialize(environments.APPLICATION_ID, environments.JAVASCRIPT_KEY);
-Parse.serverURL = environments.SERVER_URL;
-
-//imports
+//Import necessary libraries and components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as environments from './environments'; 
-import Parse from 'parse'; 
+import * as environments from './environments';
+import Parse from 'parse';
 
+// DO INITIALIZATION
+Parse.initialize(environments.APPLICATION_ID, environments.JAVASCRIPT_KEY);
+Parse.serverURL = environments.SERVER_URL;
 
-//Render App
+// RENDER APP
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -23,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
