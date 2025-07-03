b052738339b0f87c5fed834da6ba63cf0fdbcbad
Confidence Nawali <137927899+Confidence85@users.noreply.github.com>
2025-07-02 18:09:59 -0700

    Fix ESLint errors in index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import Parse from 'parse';
 Parse.initialize(environments.APPLICATION_ID, environments.JAVASCRIPT_KEY);
 Parse.serverURL = environments.SERVER_URL;
 
+//imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as environments from './environments'; 
+import Parse from 'parse'; 
 
+
+//Render App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
