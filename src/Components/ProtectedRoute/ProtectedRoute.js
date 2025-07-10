8b67b10b49a18e61b3fe7bfae17f95a733346517
Ashley Leyva <aleyva2@nd.edu>
2025-07-10 10:59:56 -0700

    Updating Protected Route file

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,10 +1,10 @@
-// Based on the Feature 5 Kickoff Solution
+// Setting up the protected route service for the app
 // import react and user info from other pages
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { checkUser } from "../Auth/AuthService";
 
-// Creating the protected route element
+// Defining the protected route element
 const ProtectedRoute = ({ element: Component, ...rest }) => {
   console.log("element: ", Component);
   const navigate = useNavigate();
