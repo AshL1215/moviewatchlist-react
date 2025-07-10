dede5bb1ca73bd47e6c4e0d4c9a6749fb1c1777e
Ashley Leyva <aleyva2@nd.edu>
2025-07-10 13:31:12 -0700

    Updated About Us Page

--- src/Components/Auth/About.js
+++ src/Components/Auth/About.js
@@ -1,15 +1,14 @@
 // This is the first page users will see when they enter website
+// This is a simple About component for the Movie Watchlist application.
+// It displays information about the application when the user visits the about page.
 // import react features and other necessary files
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { checkUser } from "./AuthService";
+import { checkUser } from "./authservices";
 
 function About() {
-  return <h1>About This App</h1>;
-}
-
-// Redirect user if they are already logged in
+  // Redirect user if they are already logged in
  useEffect(() => {
     if (checkUser()) {
       alert("You are already logged in");
@@ -17,8 +16,24 @@ function About() {
     }
   }, [navigate]);
 
-export default About
-// This is a simple About component for the Movie Watchlist application.
-// It displays information about the application when the user visits the about page.
+  return (
+  <div>
+  <h1>About This App</h1>
+  <p>
+    Our movie watchlist app allows you to explore our catalogue of movies and add them to your watchlist!
+  </p>
 
-// Need to add login and register button
+  // Login and register buttons
+      <Link to="./authregister">
+        <button>Register</button>
+      </Link>
+      <br />
+      <br />
+      <Link to="./authlogin">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+};
+
+export default About
