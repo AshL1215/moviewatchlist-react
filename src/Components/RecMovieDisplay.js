29d9cf8cfe46438f770177a701c71f405756b8ef
Ashley Leyva <aleyva2@nd.edu>
2025-07-21 22:43:48 -0700

    Resolved merge conflict in RecMovieDisplay.js

--- src/Components/RecMovieDisplay.js
+++ src/Components/RecMovieDisplay.js
@@ -1,5 +1,6 @@
 // This is the component that displays the recommended movie to the user
 import React from 'react';
+import {addMovie} from "../Models/WatchlistModel"
 
 // Check if there has been a movie reccomended
 const RecommendedMovieDisplay = ({ movie, onAddMovie }) => {
@@ -12,7 +13,7 @@ const RecommendedMovieDisplay = ({ movie, onAddMovie }) => {
             <p><strong>Title:</strong> {movie.get("title")}</p>
             <p><strong>Year:</strong> {movie.get("year")}</p>
             <p><strong>Genre:</strong> {movie.get("genre")}</p>
-            <button onClick={() => onAddMovie(movie)}>
+            <button onClick={() => addMovie(movie)}>
                 Add to Watchlist
             </button>
         </div>
