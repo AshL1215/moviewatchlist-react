ed9cb52f960e67fda623dd2dd9aa4e8e6dd2bf53
Ashley Leyva <aleyva2@nd.edu>
2025-07-21 19:22:41 -0700

    Created the Recommended Movie Display

---
+++ src/Components/RecMovieDisplay.js
@@ -0,0 +1,22 @@
+// This is the component that displays the reccomended movie to the user
+import React from 'react';
+
+// Check if there has been a movie reccomended
+const RecommendedMovieDisplay = ({ movie, onAddMovie }) => {
+    if (!movie) {
+        return null;
+    }
+
+    return (
+        <div className='RecMovie'>
+            <p><strong>Title:</strong> {movie.get("title")}</p>
+            <p><strong>Year:</strong> {movie.get("year")}</p>
+            <p><strong>Genre:</strong> {movie.get("genre")}</p>
+            <button onClick={() => onAddMovie(moive)}>
+                Add to Watchlist
+            </button>
+        </div>
+    );
+};
+
+export default RecMovieDisplay;
